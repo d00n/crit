8a8bb21324a8083a781ddade5d8162b7c8773532
Mike Muldon <mike@infrno.net>
2015-09-18 12:35:39 -0700

    fix double form post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,8 @@ class SessionsController < BaseController
     if @user_session.save
       self.current_user = @user_session.record #if current_user has been called before this, it will ne nil, so we have to make to reset it
 
-      flash[:notice] = :thanks_youre_now_logged_in.l
-      redirect_back_or_default(dashboard_user_path(current_user))
+      #flash[:notice] = :thanks_youre_now_logged_in.l
+      redirect_back_or_default(home_path)
     else
       flash[:notice] = :uh_oh_we_couldnt_log_you_in_with_the_username_and_password_you_entered_try_again.l
       render :action => :new, :email => params[:email]
