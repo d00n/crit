ad9228b31654c9ac2a6de013b22dbeed4f79446b
Mike Muldon <mike@infrno.net>
2015-10-25 02:31:34 -0700

    enable cap linked_dir defaults, push the button!

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ lock '3.4.0'
 
 set :application, 'infrno'
 set :repo_url, 'git@github.com:d00n/crit.git'
-
 set :passenger_restart_with_touch, true
 
 set :branch, $1 if `git branch` =~ /\* (\S+)\s/m
@@ -43,7 +42,7 @@ set :log_level, :debug
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
