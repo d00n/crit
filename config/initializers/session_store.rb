f41e37630897a09165d6333e171e7e16e867b9c9
Mike Muldon <mike@infrno.net>
2015-11-15 02:21:10 -0800

    cookie cleanup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Crit::Application.config.session_store :cookie_store, key: ' infrno_session'
+Crit::Application.config.session_store :cookie_store, key: 'crit2_session'
