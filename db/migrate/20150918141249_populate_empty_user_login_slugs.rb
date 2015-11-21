60364ce15306854e4bd3a554671fdc18c345dd21
Mike Muldon <mike@infrno.net>
2015-11-21 07:50:05 -0800

    prep for prod db copy

--- db/migrate/20150918141249_populate_empty_user_login_slugs.rb
+++ db/migrate/20150918141249_populate_empty_user_login_slugs.rb
@@ -9,8 +9,11 @@ class PopulateEmptyUserLoginSlugs < ActiveRecord::Migration
     if User.exists?(11931)
       User.find(11931).destroy
     end
+    if User.exists?(17869)
+      User.find(17869).destroy
+    end
     User.where("login_slug is null").each do |u|
-      puts "> #{u.id}, #{u.login}, #{u.email}"
+      puts "> null login_slug: #{u.id}, #{u.login}, #{u.email}"
       u.login_slug = u.login.tr(" ", "_")
       u.save!
     end
