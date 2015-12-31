4381169dd7657950f1595820a275185c5e77854a
Mike Muldon <mike@infrno.net>
2015-12-30 18:33:14 -0800

    cache test

--- app/controllers/post_sweeper.rb
+++ app/controllers/post_sweeper.rb
@@ -3,6 +3,7 @@ class PostSweeper < ActionController::Caching::Sweeper
   private
   def expire_cache_for(record)
     cache_key = 'views/#{request.host_with_port}/index'
+    log(:debug, "cache_key:#{cache_key}")
     Rails.cache.delete(cache_key)
   end
 end
