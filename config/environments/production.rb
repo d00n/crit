7beb4759c06744b70745ea83b8ef4d914db06c38
Mike Muldon <mike@infrno.net>
2015-12-31 12:29:02 -0800

    flip APP_URL to prod setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Crit::Application.configure do
 
   config.action_mailer.smtp_settings = { :enable_starttls_auto => false }
 
-  APP_URL = "http://crit.infrno.net"
+  APP_URL = "http://www.infrno.net"
 
   LIBRARY_SERVER = "http://www.infrno.net"
   LIBRARY_BASE_PATH = "/libraries/"
