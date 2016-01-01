d5ebb7e1c66cfa57ee9581b97cbe8a0503a7d175
Mike Muldon <mike@infrno.net>
2015-12-31 17:01:32 -0800

    tinymce tweaks

--- config/initializers/mce_options.rb
+++ config/initializers/mce_options.rb
@@ -5,7 +5,7 @@ configatron.default_mce_options = {
     :convert_fonts_to_spans => true,
     :menubar => false,
     :statusbar => false,
-    :toolbar => 'undo redo code | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link unlink image media',
+    :toolbar => 'code undo redo | bold italic | alignleft aligncenter alignright| bullist link unlink image media',
     :plugins => %w{media link image preview autosave emoticons paste autoresize code table},
     :extended_valid_elements => "img[class|src|flashvars|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|obj|param|embed|scale|wmode|salign|style],embed[src|quality|scale|salign|wmode|bgcolor|width|height|name|align|type|pluginspage|flashvars],object[align<bottom?left?middle?right?top|archive|border|class|classid|codebase|codetype|data|declare|dir<ltr?rtl|height|hspace|id|lang|name|style|tabindex|title|type|usemap|vspace|width]",
     :media_strict => false
@@ -18,6 +18,6 @@ configatron.simple_mce_options = {
     :convert_fonts_to_spans => true,
     :menubar => false,
     :statusbar => false,
-    :toolbar => 'bold italic| bullist numlist | link unlink image',
+    :toolbar => 'bold italic| bullist | link unlink image',
     :plugins => %w{autosave link image emoticons },
 }
