a481cbd68a7e35eb49517cd4a9b2eea7ec76e201
Mike Muldon <mike@infrno.net>
2016-01-10 05:47:44 -0800

    guard rail for /sb_posts.rss

--- app/controllers/sb_posts_controller.rb
+++ app/controllers/sb_posts_controller.rb
@@ -1,6 +1,26 @@
 class SbPostsController < BaseController
   require_from_ce('controllers/sb_posts_controller')
 
+  def index
+
+    respond_to do |format|
+      format.rss {
+        render :nothing => true
+        return
+      }
+    end
+
+    conditions = []
+    [:user_id, :forum_id].each { |attr|
+      conditions << SbPost.send(:sanitize_sql, ["sb_posts.#{attr} = ?", params[attr].to_i]) if params[attr]
+    }
+    conditions = conditions.any? ? conditions.collect { |c| "(#{c})" }.join(' AND ') : nil
+
+    @posts = SbPost.with_query_options.where(conditions).page(params[:page])
+
+    @users = User.distinct.where(:id => @posts.collect(&:user_id).uniq).to_a.index_by(&:id)
+  end
+
   protected
     #overide in your app
     def authorized?
