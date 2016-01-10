37e5589710e41db771257ff7139c524633cb87c8
Mike Muldon <mike@infrno.net>
2016-01-10 08:43:59 -0800

    scanner countermeasures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   protect_from_forgery
+
+  def raise_not_found
+    respond_to do |format|
+      format.all {
+        render :nothing => true, status: 404
+      }
+    end
+  end
+
+  rescue_from ActionController::UnknownController, with: -> { raise_not_found  }
+  rescue_from ActiveRecord::RecordNotFound, with: -> { raise_not_found  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -238,4 +238,6 @@ Crit::Application.routes.draw do
     resources :comments, :as => :commentable_comments
   end
 
+  get '*php', :to => 'application#raise_not_found'
+
 end
